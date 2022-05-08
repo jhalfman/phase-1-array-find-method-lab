0f0c1ff90695011abe6ba464752b6085b27b6acb
Jordan Halfman <jordanhalfman@gmail.com>
2022-05-08 13:04:56 -0700

    commit tree 74b333ed017c92090eafeea5fde147d1fdb41344

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function superbowlWin(history) {
+    const year = history.find((stat) => stat["result"] === "W");
+    if (year) {
+    return year["year"];
+    }
+    else {
+        return undefined;
+    }
+}
+
